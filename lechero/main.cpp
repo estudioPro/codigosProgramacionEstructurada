e03b7362cd22ba240892ed05f2287bb0f0589e29
Vandersal Aheram <163239@ids.upchiapas.edu.mx>
2019-10-15 17:53:25 -0500

    Codigo del dia 14/10/19

---
+++ lechero/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main() {
+    float litrosProduccion;
+    float precioGalon;
+    float cantidadGalonProduccion;
+    float ganancia;
+    cout<<"Ingresa la cantidad de litros en produccion: ";
+    cin>>litrosProduccion;
+    cout<<"Ingresa el precio del galon: ";
+    cin>>precioGalon;
+    cantidadGalonProduccion = (litrosProduccion / 3.785);
+    ganancia = precioGalon*cantidadGalonProduccion;
+
+    cout<<"Tu ganancia total es: "<<ganancia<<endl;
+
+    return 0;
+}
