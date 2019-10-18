ba312e73780f0e29039bc41fe1cecbbabe224a21
Vandersal Aheram Archila Chua <163239@ids.upchiapas.edu.mx>
2019-10-18 15:16:18 -0500

    Cree la estructura basica del programa

---
+++ Algoritmo13/cmake-build-debug/CMakeFiles/Algoritmo13.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Algoritmo13.exe"
+  "Algoritmo13.exe.manifest"
+  "Algoritmo13.pdb"
+  "CMakeFiles/Algoritmo13.dir/main.cpp.obj"
+  "libAlgoritmo13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algoritmo13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Algoritmo13/cmake-build-debug/CMakeFiles/Algoritmo13.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Algoritmo13.
+# This may be replaced when dependencies are built.
